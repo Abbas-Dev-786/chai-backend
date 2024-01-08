8b501a0fddc941178e2d0b9bc8e364bb62c9f1d5
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2024-01-08 09:43:03 +0530

    first commit

---
+++ src/utils/ApiError.js
@@ -0,0 +1,25 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+    this.isOperational = true;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
